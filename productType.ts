9273b7c904c98ca4e8878dbcb5daefa3105b9f3c
Khaagic <khimkhunkha@gmail.com>
2025-07-23 01:39:41 +0700

    Add files via upload

---
+++ productType.ts
@@ -0,0 +1,37 @@
+export type ProductType = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    thumbnail: string;
+    category: string;
+}
+export type CardItems = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    thumbnail: string;
+    category: string;
+}
+
+
+export type ProductDetailType = {
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    thumbnail: string;
+    discountPercentage: number;
+    stock: number;
+    category: string;
+    reviews: Reviews[];
+}
+
+export type Reviews = {
+    rating: number;
+    comment: string;
+    date: number;
+    recieverName: string;
+    reviewerEmail: string;
+}
